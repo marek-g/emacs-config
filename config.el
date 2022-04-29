2969656008c7c2fe4195271e665ebf5405a26a30
Marek G <marek-dev@yandex.com>
2022-04-29 22:05:03 +0200

    Home key - cycle between move to beginning of line and indentation

--- config.el
+++ config.el
@@ -155,7 +155,22 @@
 (setq mouse-wheel-progressive-speed nil)
 
 ;;
+;; home key - cycle between move to beginning of line and indentation
+;;
+(defun smart-beginning-of-line()
+  "Move point to first non-whitespace character or beginning-of-line.
 
+Move point to the first non-whitespace character on this line.
+If point was already at that position, move point to beginning of line."
+  (interactive "^") ; Use (interactive) in Emacs 22 or older
+  (let ((oldpos (point)))
+    (back-to-indentation)
+    (and (= oldpos (point))
+         (beginning-of-line))))
+
+(global-set-key [home] 'smart-beginning-of-line)
+
+;;
 ;; treemacs configuration
 ;;
 
