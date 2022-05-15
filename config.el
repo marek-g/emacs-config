cf41b98fb2a9c43c45fc17448a1c0b00ebcf2131
Marek G <marek-dev@yandex.com>
2022-05-15 21:04:37 +0200

    C-w write & close, C-k kill buffer

--- config.el
+++ config.el
@@ -106,7 +106,11 @@
   :config
   (wakib-keys 1)
   (add-hook 'after-change-major-mode-hook 'wakib-update-major-mode-map)
-  (add-hook 'menu-bar-update-hook 'wakib-update-minor-mode-maps))
+  (add-hook 'menu-bar-update-hook 'wakib-update-minor-mode-maps)
+  ; write & close - this works with commits in magit
+  (define-key wakib-keys-overriding-map (kbd "C-w") 'with-editor-finish)
+  ; killing the buffer is moved to C-k
+  (define-key wakib-keys-overriding-map (kbd "C-k") 'kill-current-buffer))
 
 ;;
 ;; Switch active window (ctrl+<tab>)
