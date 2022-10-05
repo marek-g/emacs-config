818b685c46cd5a63541b08eeeebaac459ad464dd
Marek G <marek-dev@yandex.com>
2022-10-05 12:30:59 +0200

    Orgmode - enable shift to select text

--- config.el
+++ config.el
@@ -44,10 +44,6 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
@@ -83,10 +79,12 @@
 
 
 ;;
-;; Dashboard customization
+;; External configs
 ;;
 
+; dashboard customization
 (load! "dashboard.el")
+(load! "orgmode.el")
 
 
 ;;
---
+++ orgmode.el
@@ -0,0 +1,10 @@
+;;
+;; Org-mode specific configuration.
+;;
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; allow to select text with shift
+(setq org-support-shift-select t)
