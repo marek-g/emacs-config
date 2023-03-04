f8b094e81e5288bcd9d6f41b592ec0819ca7c14e
Marek G <marek-dev@yandex.com>
2023-03-04 15:06:29 +0100

    Fix termux path on Android

--- early-init.el
+++ early-init.el
@@ -29,5 +29,5 @@
 (when ANDROID-P
   ;; Add Termux binaries to PATH environment
   (let ((termuxpath "/data/data/com.termux/files/usr/bin"))
-    (setenv "PATH" (concat (getenv "PATH") termuxpath))
+    (setenv "PATH" (concat (getenv "PATH") ":" termuxpath))
     (setq exec-path (append exec-path (list termuxpath)))))
