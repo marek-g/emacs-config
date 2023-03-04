bf44717b0c4330627bcf661e681540d7819d38b1
Marek G <marek-dev@yandex.com>
2023-03-04 18:52:23 +0100

    Enable package.el and use-package

---
+++ init.el
@@ -0,0 +1,2 @@
+;; initilize package manager
+(load (message (expand-file-name "package-manager-init.el" user-emacs-directory)))
---
+++ package-manager-init.el
@@ -0,0 +1,24 @@
+;; use standard package manager (package.el)
+(require 'package)
+
+;; add `melpa` to `package-archives`
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+;; manually manage list of selected packages, so they are not added automatically to init.el
+;; you can install packages with (package-install-selected-packages)
+;; you can remove packages with (package-autoremove)
+(setq package-selected-packages
+  '(use-package))
+
+;; initialize package manager
+(package-initialize)
+
+;; install use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; causes the packages to be installed automatically if not already present on your system
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
