cc151605918fe1e9881dd06e042daca37f5ee6b2
Marek G <marek-dev@yandex.com>
2023-03-06 17:15:21 +0100

    Add Readonly option

--- modules/marek-popup-menu.el
+++ modules/marek-popup-menu.el
@@ -3,4 +3,15 @@
   :config
   (global-set-key [mouse-3] (popup-edit-menu-stub)))
 
+; Readonly option
+(define-key-after global-map [menu-bar edit read-only-mode] `("Readonly" . read-only-mode) 'fill)
+; TODO: checkbox is not working - but it is for example for blink-cursor-mode
+;(define-key-after global-map [menu-bar edit read-only-mode]
+;  (menu-bar-make-mm-toggle
+;   	read-only-mode
+;	"Readonly"
+;	"Enable read-only-mode"
+;  )
+;  'fill)
+
 (provide 'marek-popup-menu)
