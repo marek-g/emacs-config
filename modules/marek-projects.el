6c9a633a3045f5239d0ac0bfd6993d8cb6e69fc0
Marek G <marek-dev@yandex.com>
2023-03-06 18:44:37 +0100

    Add projectile

--- modules/marek-projects.el
+++ modules/marek-projects.el
@@ -10,4 +10,21 @@
         ("<f9>" . treemacs))
 )
 
+(add-to-list 'package-selected-packages 'projectile)
+(use-package projectile
+  :init
+  (setq projectile-cache-file (expand-file-name ".cache/projectile.cache" user-emacs-directory)
+	projectile-known-projects-file (expand-file-name ".cache/projectile-known-projects.eld" user-emacs-directory))
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("C-c p" . projectile-command-map)))
+
+(add-to-list 'package-selected-packages 'treemacs-projectile)
+(use-package treemacs-projectile
+  :after (treemacs projectile))
+
+(add-to-list 'package-selected-packages 'treemacs-magit)
+(use-package treemacs-magit
+  :after (treemacs magit))
+
 (provide 'marek-projects)
