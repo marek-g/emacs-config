b4c9b64c5282ac82d7e58f4837b333398b018ee7
Marek G <marek-dev@yandex.com>
2023-03-06 18:00:00 +0100

    Enable tabs and line wrapping

--- modules/marek-general-settings.el
+++ modules/marek-general-settings.el
@@ -3,4 +3,10 @@
 ;;
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
+(setq-default indent-tabs-mode t) ; enable tabs
+(global-visual-line-mode t) ; enable long line wrapping
+;(global-linum-mode t) ; enable line numbers
+;(when (fboundp 'set-scroll-bars-mode)
+;  (set-scroll-bar-mode t)) ; enable scroll bars
+
 (provide 'marek-general-settings)
