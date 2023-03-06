a8047ace438aa658eb2669cc4ead41022a814853
Marek G <marek-dev@yandex.com>
2023-03-06 17:29:36 +0100

    Add vundo

--- modules/marek-general-keys.el
+++ modules/marek-general-keys.el
@@ -16,6 +16,24 @@
   (define-key wakib-keys-overriding-map (kbd "s-k") 'delete-window))
 
 ;;
+;; vundo (visual undo-tree)
+;;
+(add-to-list 'package-selected-packages 'vundo)
+(use-package vundo
+  :config
+  (setq vundo-compact-display t)
+  (setq vundo-glyph-alist vundo-unicode-symbols)
+  (custom-set-faces
+    '(vundo-node ((t (:foreground "#808080"))))
+    '(vundo-stem ((t (:foreground "#808080"))))
+    '(vundo-highlight ((t (:foreground "#FFFF00")))))
+)
+
+; redo
+(global-set-key (kbd "C-S-z") 'vundo)
+(global-set-key (kbd "C-y") 'undo-redo)
+
+;;
 ;; <menu> leader key key-map
 ;;
 ;; on Linux, the menu/apps key syntax is <menu>
