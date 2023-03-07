1926f1143b79e3ee4504665ac8ca9a9d803cceea
Marek G <marek-dev@yandex.com>
2023-03-07 19:44:11 +0100

    Set default font to Iosevka 16

--- modules/marek-theme.el
+++ modules/marek-theme.el
@@ -3,4 +3,15 @@
 ;;
 (load-theme 'deeper-blue t)
 
+;;
+;; Default font
+;;
+
+(when DESKTOP-P
+  (let ((my-font (font-spec :family "Iosevka" :weight 'normal :width 'expanded :size 16)))
+    (unless (find-font my-font) (setq my-font (font-spec :size 16)))
+    (set-frame-font my-font nil t)
+  )
+)
+
 (provide 'marek-theme)
