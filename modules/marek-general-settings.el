04ef92f11d4bc6d568b880b72a64ebfa19fbde85
Marek G <marek-dev@yandex.com>
2023-03-07 13:12:12 +0100

    Enable delete-selection-mode

--- modules/marek-general-settings.el
+++ modules/marek-general-settings.el
@@ -3,6 +3,7 @@
 ;;
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
+(delete-selection-mode t) ; enable deleting selected text when typing
 (setq-default indent-tabs-mode t) ; enable tabs
 (global-visual-line-mode t) ; enable long line wrapping
 ;(global-linum-mode t) ; enable line numbers
