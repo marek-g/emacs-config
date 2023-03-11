a946c93d9a87412096583088ed3f9c0f484f2e83
Marek G <marek-dev@yandex.com>
2023-03-11 21:02:35 +0100

    Resign from C-w shortcut (with-editor-finish)

--- modules/marek-general-keys.el
+++ modules/marek-general-keys.el
@@ -5,8 +5,6 @@
   (wakib-keys 1)
   (add-hook 'after-change-major-mode-hook 'wakib-update-major-mode-map)
   (add-hook 'menu-bar-update-hook 'wakib-update-minor-mode-maps)
-  ; write & close - this works with commits in magit
-  (define-key wakib-keys-overriding-map (kbd "C-w") 'with-editor-finish)
   ; killing the buffer is moved to C-k
   (define-key wakib-keys-overriding-map (kbd "C-k") 'kill-current-buffer)
   ; split window horizontally
