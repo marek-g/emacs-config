c7a0b28fbffec16726d49a67c31644680139e6e6
Marek G <marek-dev@yandex.com>
2023-03-11 18:05:27 +0100

    Emacs on Android binary from sourceforge doesn't need gnutls-cli

--- early-init.el
+++ early-init.el
@@ -26,16 +26,4 @@
   (let ((termuxpath "/data/data/com.termux/files/usr/bin"))
     (setenv "PATH" (concat (getenv "PATH") ":" termuxpath))
     (setq exec-path (append exec-path (list termuxpath))))
-
-  ;; Fix SSL connection.
-  ;;
-  ;; Android port is builded without gnutls support and uses 'gnutls-cli' command.
-  ;; 'gnutls' command line must be installed in Termux.
-  ;; This removes '%t' (filepath with trused certificates) from original command line, as it causes the error.
-  ;; "--x509cafile /data/data/com.termux/files/usr/etc/tls/cert.pem' was also not working".
-  ;; 'openssl' is not working at all.
-  (setq tls-program '("gnutls-cli -p %p %h"
-		      "gnutls-cli -p %p %h --protocols ssl3"
-		      ;"openssl s_client -connect %h:%p -no_ssl2 -ign_eof"
-  ))
 )
