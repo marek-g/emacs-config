395a024b5a59c07a7b10839becab778474f11769
Marek G <marek-dev@yandex.com>
2023-03-14 20:59:43 +0100

    Show column number and hide toolbar

--- modules/marek-general-settings.el
+++ modules/marek-general-settings.el
@@ -6,8 +6,10 @@
 (delete-selection-mode t) ; enable deleting selected text when typing
 (setq-default indent-tabs-mode t) ; enable tabs
 (global-visual-line-mode t) ; enable long line wrapping
+(column-number-mode 1) ; show column in mode line
 ;(global-linum-mode t) ; enable line numbers
 ;(when (fboundp 'set-scroll-bars-mode)
 ;  (set-scroll-bar-mode t)) ; enable scroll bars
+(tool-bar-mode -1) ; hide toolbar
 
 (provide 'marek-general-settings)
