52a27c84de912e276316250f060613a9826ea99e
Marek G <marek-dev@yandex.com>
2023-03-15 22:47:50 +0100

    Enable dap-mode for python

--- modules/marek-lsp-python.el
+++ modules/marek-lsp-python.el
@@ -1,8 +1,11 @@
 (add-to-list 'package-selected-packages 'lsp-pyright)
 (use-package lsp-pyright
-  :ensure t
   :hook (python-mode . (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp-deferred))))
+                         (require 'lsp-pyright)
+			 (require 'dap-python)
+			 ;; if you installed debugpy, you need to set this
+			 ;; https://github.com/emacs-lsp/dap-mode/issues/306
+			 (setq dap-python-debugger 'debugpy)
+                         (lsp-deferred))))
 
 (provide 'marek-lsp-python)
