14245ebbb74eb4ba19f5fe7528f24716bf6f9183
Marek G <marek-dev@yandex.com>
2023-03-19 21:38:29 +0100

    Hide scroll bar from mini bar

--- modules/marek-general-settings.el
+++ modules/marek-general-settings.el
@@ -7,9 +7,11 @@
 (setq-default indent-tabs-mode t) ; enable tabs
 (global-visual-line-mode t) ; enable long line wrapping
 (column-number-mode 1) ; show column in mode line
-;(global-linum-mode t) ; enable line numbers
-;(when (fboundp 'set-scroll-bars-mode)
-;  (set-scroll-bar-mode t)) ; enable scroll bars
+
+;;(global-linum-mode t) ; enable line numbers
+
+(set-window-scroll-bars (minibuffer-window) nil nil) ; hide scroll bar from mini-buffer
+
 (tool-bar-mode -1) ; hide toolbar
 
 (provide 'marek-general-settings)
