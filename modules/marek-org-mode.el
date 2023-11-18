537baae10f08cff5d23bbaa5c5102d5417a911af
Marek G <marek-dev@yandex.com>
2023-11-18 17:18:54 +0100

    Fix todo.org path

--- modules/marek-org-mode.el
+++ modules/marek-org-mode.el
@@ -7,7 +7,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (when (string-equal (system-name) "marek-linux")
-  (setq org-directory "/media/veracrypt1/dokumenty/Notatki/private_notes/Todo/"))
+  (setq org-directory "~/Ext/dokumenty/Notatki/private_notes/Todo/"))
 
 (setq org-agenda-files (list (concat org-directory "Todo.org")))
 
