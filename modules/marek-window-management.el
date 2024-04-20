cc8dd21bddadb4ef59d6230b94d9e68d40953729
Marek G <marek-dev@yandex.com>
2024-04-20 14:19:40 +0200

    other-window / prev-window

--- modules/marek-window-management.el
+++ modules/marek-window-management.el
@@ -1,8 +1,18 @@
+;; ace-window
 (add-to-list 'package-selected-packages 'ace-window)
 (use-package ace-window
   :init
   (global-set-key (kbd "C-<tab>") 'ace-window))
 
+;; other-window / previous-window
+(global-set-key (kbd "C-.") #'other-window)
+(global-set-key (kbd "C-,") #'prev-window)
+
+(defun prev-window ()
+  (interactive)
+  (other-window -1))
+
+;; burly
 (add-to-list 'package-selected-packages 'burly)
 (use-package burly)
 
