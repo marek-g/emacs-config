dfbe237afb256640165ebb811c35e48da2222062
Marek G <marek-dev@yandex.com>
2024-07-14 18:26:14 +0200

    ace-window: another <tab> to select the previous window

--- modules/marek-window-management.el
+++ modules/marek-window-management.el
@@ -2,7 +2,25 @@
 (add-to-list 'package-selected-packages 'ace-window)
 (use-package ace-window
   :init
-  (global-set-key (kbd "C-<tab>") 'ace-window))
+  (global-set-key (kbd "C-<tab>") 'ace-window)
+  :config
+  ;; changes: another <tab> to select the previous window
+  (setq aw-dispatch-alist '((?x aw-delete-window "Delete Window")
+			    (?m aw-swap-window "Swap Windows")
+			    (?M aw-move-window "Move Window")
+			    (?c aw-copy-window "Copy Window")
+			    (?j aw-switch-buffer-in-window "Select Buffer")
+			    (?n aw-flip-window)
+			    (?\t aw-flip-window)
+			    (?u aw-switch-buffer-other-window "Switch Buffer Other Window")
+			    (?e aw-execute-command-other-window "Execute Command Other Window")
+			    (?F aw-split-window-fair "Split Fair Window")
+			    (?v aw-split-window-vert "Split Vert Window")
+			    (?b aw-split-window-horz "Split Horz Window")
+			    (?o delete-other-windows "Delete Other Windows")
+			    (?T aw-transpose-frame "Transpose Frame")
+			    ;; ?i ?r ?t are used by hyperbole.el
+			    (?? aw-show-dispatch-help)))
 
 ;; other-window / previous-window
 (global-set-key (kbd "C-.") #'other-window)
