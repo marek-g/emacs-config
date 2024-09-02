1d46cefdfaceead52c9c2330e49c7ca255a558b1
Marek G <marek-dev@yandex.com>
2024-09-02 23:11:31 +0200

    Improve help

--- modules/marek-help.el
+++ modules/marek-help.el
@@ -1,32 +1,23 @@
 (define-key global-map [f1] 'marek-help-toggle-window)
 
-(defun marek-help-toggle-window ()
+(defun marek-help-toggle-window()
   "Open or close marek-help window"
   (interactive)
-  (if (get-buffer "*marek-help*")
+  (if (get-file-buffer (expand-file-name "help/main.org" user-emacs-directory))
       (marek-help-close)
     (marek-help-show)))
 
 ;;;###autoload
-(defun marek-help-close ()
+(defun marek-help-close()
   "Close marek-help window"
   (interactive)
-  (if (not (string-equal "*marek-help*" (buffer-name)))
-      (setq marek-help-buffer-to-return (buffer-name)))
-  (switch-to-buffer-other-window "*marek-help*")
-  (kill-buffer-and-window)
-  (switch-to-buffer marek-help-buffer-to-return))
+  (kill-buffer (get-file-buffer (expand-file-name "help/main.org" user-emacs-directory))))
 
 ;;;###autoload
-(defun marek-help-show ()
+(defun marek-help-show()
   "Create buffer and show marek-help."
   (interactive)
-  (setq marek-help-buffer-to-return (buffer-name))
-  (switch-to-buffer-other-window "*marek-help*")
-  (org-mode)
-  (erase-buffer)
-  (insert-file-contents (expand-file-name "help/main.org" user-emacs-directory))
-  (goto-char 0)
+  (find-file (expand-file-name "help/main.org" user-emacs-directory))
   (setq buffer-read-only t))
 
 (provide 'marek-help)
