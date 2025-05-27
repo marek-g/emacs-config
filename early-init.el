5e0bb0330aaaf4829fca6184d8e8e7efc637489e
Marek G <marek@nomail.org>
2025-05-27 13:18:49 +0200

    Android: fix termux paths

--- early-init.el
+++ early-init.el
@@ -25,8 +25,8 @@
 (when ANDROID-P
   ;; Add Termux binaries to PATH environment
   (let ((termuxpath "/data/data/com.termux/files/usr/bin"))
-    (setenv "PATH" (concat (getenv "PATH") ":" termuxpath))
-    (setq exec-path (append exec-path (list termuxpath))))
+    (setenv "PATH" (format "%s:%s" termuxpath (getenv "PATH")))
+    (push termuxpath exec-path))
 )
 
 ;;
