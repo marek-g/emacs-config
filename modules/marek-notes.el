86655edd01f1b22f97ef1c6f2fe03eebbba85ed3
Marek G <marek-dev@yandex.com>
2025-07-13 23:39:05 +0200

    Add denote notes to dashboard

--- modules/marek-notes.el
+++ modules/marek-notes.el
@@ -4,10 +4,45 @@
 ;; Intended for persistent but temporary notes.
 ;;
 
+;;
+;; Configure denote
+;;
+
 (add-to-list 'package-selected-packages 'denote)
 (use-package denote
   :config
   (setq denote-directory (expand-file-name "~/Documents/notes/")))
+
+
+;;
+;; Add denote notes to the dashboard
+;;
+
+(require 'denote)
+(require 'dashboard)
+
+(defun my/dashboard-denote-notes (list-size)
+  "Return a list of Denote note titles for the dashboard."
+  (let ((files (denote-directory-files)))
+    (mapcar (lambda (file)
+              (list
+               :title (denote-retrieve-filename-title file)
+               :file  file))
+            (dashboard-subseq (reverse files) list-size))))
+
+(defun my/dashboard-insert-denote-widget (list-size)
+  "Insert a widget showing recent Denote notes into the dashboard."
+  (dashboard-insert-section
+   "Denote Notes:"
+   (my/dashboard-denote-notes list-size)
+   list-size
+   'notes
+   "d" ;;(dashboard-get-shortcut 'notes)
+   `(lambda (&rest _) (find-file (plist-get ',el :file)))
+   (format "%s" (plist-get el :title))))
+
+(add-to-list 'dashboard-item-generators '(denote . my/dashboard-insert-denote-widget))
+(add-to-list 'dashboard-items '(denote . 5))
 
 (provide 'marek-notes)
 
