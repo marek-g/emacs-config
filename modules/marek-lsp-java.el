755fd15ad94a8d26ff943a5a0ee0cde92d3f7fd1
Marek G <marek-dev@yandex.com>
2025-07-17 16:24:26 +0200

    Reenable Java LSP at work

--- modules/marek-lsp-java.el
+++ modules/marek-lsp-java.el
@@ -1,4 +1,6 @@
 (add-to-list 'package-selected-packages 'lsp-java)
-(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
+(use-package lsp-java
+  ;;:config (add-hook 'java-mode-hook 'lsp)
+)
 
 (provide 'marek-lsp-java)
