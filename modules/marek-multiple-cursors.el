461d3c3ede42d0ec1b2d337fd040eb448357d307
Marek G <marek-dev@yandex.com>
2025-12-14 14:24:05 +0100

    Fix enter in multiple-cursors

--- modules/marek-multiple-cursors.el
+++ modules/marek-multiple-cursors.el
@@ -1,15 +1,11 @@
 (add-to-list 'package-selected-packages 'multiple-cursors)
-(add-to-list 'package-selected-packages 'multiple-cursors-core)
 (use-package multiple-cursors
   :bind (("C-S-c C-S-c" . mc/edit-lines) ;; turn a selection into multiple cursors, one per line
          ("C->"         . mc/mark-next-like-this) ;; add cursors for next matching text
          ("C-<"         . mc/mark-previous-like-this) ;; add cursors for previous matching text
-         ("C-c C-<"     . mc/mark-all-like-this))) ;; mark all matches in the buffer
-
-;; let <return> insert <return> in multiple-cursors mode
-(use-package multiple-cursors-core
-    :bind
+         ("C-c C-<"     . mc/mark-all-like-this)) ;; mark all matches in the buffer
    (:map mc/keymap
-         ("<return>" . nil)))
+         ("<return>" . nil)) ;; let <return> insert <return> in multiple-cursors mode
+)
 
 (provide 'marek-multiple-cursors)
