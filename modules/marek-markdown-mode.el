0d92d10830efce84116b34bbc6c9b2b42820afe0
Marek G <marek-dev@yandex.com>
2026-03-31 09:31:44 +0200

    Disable hide markdown syntax

--- modules/marek-markdown-mode.el
+++ modules/marek-markdown-mode.el
@@ -17,41 +17,40 @@
   (markdown-header-face-5 ((t (:height 1.1 :inherit markdown-header-face))))
   (markdown-header-face-6 ((t (:height 1.05 :inherit markdown-header-face))))
 
-  
   ;;
   ;; automatically add hook to hide/unhide markdown syntax
   ;;
-  :hook (markdown-mode . nb/markdown-unhighlight)
-  :config
-  (defvar-local nb/current-line nil
-    "(start . end) of current line in current buffer")
-  (defun nb/refontify-on-linemove ()
-    "Post-command-hook to trigger re-fontification when moving lines."
-    (let ((start (line-beginning-position))
-          (end (line-beginning-position 2)))
-      (unless (equal nb/current-line (cons start end))
-        (let ((old-line nb/current-line))
-          (setq nb/current-line (cons start end))
-          (when old-line
-            (font-lock-flush (car old-line) (cdr old-line)))
-          (font-lock-flush start end)))))
-  (defun nb/unhide-current-line (limit)
-    "Function to remove hiding on the current line."
-    (when (consp nb/current-line)
-      (let ((start (max (point) (car nb/current-line)))
-            (end (min limit (cdr nb/current-line))))
-        (when (< start end)
-          (remove-text-properties start end
-                                  '(invisible t display "" composition ""))
-          (goto-char limit)
-          t))))
-  (defun nb/markdown-unhighlight ()
-    "Enable markdown concealing with current-line expansion."
-    (interactive)
-    (markdown-toggle-markup-hiding 1)
-    (font-lock-add-keywords nil '((nb/unhide-current-line)) t)
-    (add-hook 'post-command-hook #'nb/refontify-on-linemove nil t)
-	)
-  )
+  ;; :hook (markdown-mode . nb/markdown-unhighlight)
+  ;; :config
+  ;; (defvar-local nb/current-line nil
+  ;;   "(start . end) of current line in current buffer")
+  ;; (defun nb/refontify-on-linemove ()
+  ;;   "Post-command-hook to trigger re-fontification when moving lines."
+  ;;   (let ((start (line-beginning-position))
+  ;;         (end (line-beginning-position 2)))
+  ;;     (unless (equal nb/current-line (cons start end))
+  ;;       (let ((old-line nb/current-line))
+  ;;         (setq nb/current-line (cons start end))
+  ;;         (when old-line
+  ;;           (font-lock-flush (car old-line) (cdr old-line)))
+  ;;         (font-lock-flush start end)))))
+  ;; (defun nb/unhide-current-line (limit)
+  ;;   "Function to remove hiding on the current line."
+  ;;   (when (consp nb/current-line)
+  ;;     (let ((start (max (point) (car nb/current-line)))
+  ;;           (end (min limit (cdr nb/current-line))))
+  ;;       (when (< start end)
+  ;;         (remove-text-properties start end
+  ;;                                 '(invisible t display "" composition ""))
+  ;;         (goto-char limit)
+  ;;         t))))
+  ;; (defun nb/markdown-unhighlight ()
+  ;;   "Enable markdown concealing with current-line expansion."
+  ;;   (interactive)
+  ;;   (markdown-toggle-markup-hiding 1)
+  ;;   (font-lock-add-keywords nil '((nb/unhide-current-line)) t)
+  ;;   (add-hook 'post-command-hook #'nb/refontify-on-linemove nil t)
+  ;; 	)
+  ;; )
 
 (provide 'marek-markdown-mode)
