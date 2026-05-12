f7c109f3141addcd9e0f9d4e90796ee1f4117579
Marek G <marek-dev@yandex.com>
2026-05-12 19:03:46 +0200

    Use environemnt variables (like PATH) from shell (bash, zsh, fish)

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ;; initilize package manager
 (load (expand-file-name "package-manager-init.el" user-emacs-directory))
 
+;; use environemnt variables (like PATH) from shell (bash, zsh, fish)
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns x pgtk))
+    (exec-path-from-shell-initialize)))
+
 ;; load all modules
 (load (expand-file-name "modules-load.el" user-emacs-directory))
